a8208b6c73b421bf9ccca1d605146274560ab5ee
vimalinivi <vimalinivedidajan5@gmail.com>
2024-07-11 19:11:04 +0530

    First time

---
+++ src/test/java/com/test/hcl/ClassA.java
@@ -0,0 +1,17 @@
+package com.test.hcl;
+
+public class ClassA {
+	private void method1() {
+		System.out.println("method 1");
+		
+	}
+	private void method2() {
+		System.out.println("method 2");
+
+	}
+	private void method3() {
+		System.out.println("method 3");
+	}
+	
+
+}
